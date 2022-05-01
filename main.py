125c059855ca81300737de21da2367c59db5f6a4
Kevin Boucinha <kevinboucinha@gmail.com>
2022-04-30 22:50:50 -0300

    updating files

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 REALM = ""
-BASE_API_URL = ""
+BASE_API_URL = "http://localhost:8000"
 BASE_KEYCLOAK_URL = ""
+
+class LoginRequest(BaseModel):
+    client_id: str
+    username: str
+    password: str
+    grant_type: str
+
+class LoginResponse(BaseModel):
+    token_type: str
+    access_token: str
+    expires_in: str
+    refresh_token: str
+    referesh_expires_in: int
 
 
 app = FastAPI()
@@ -12,7 +27,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    item = {"Hello": "World"}
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=item)
 
 
 @app.get("/items/{item_id}")
@@ -20,36 +36,38 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.post("{BASE_API_URL}/login")
-def login():
+@app.post("/login", response_model=LoginResponse)
+def login(body: LoginRequest):
+    # resp_keycloak = fetch(url_keycloak)
+    #return LoginResponse
     pass
 
 
-@app.post("{BASE_API_URL}/users")
+@app.post("/users")
 def create_user():
-    pass
+    return {"Hello": "Kevin"}
 
 
-@app.get("{BASE_API_URL}/users")
+@app.get("/users")
 def get_all_users():
-    pass
+    return {"Hello": "Kevin"}
 
 
-@app.get("{BASE_API_URL}/users/{id}")
+@app.get("/users/{id}")
 def get_user_by_id(user_id: int):
     pass
 
 
-@app.put("{BASE_API_URL}/users/{id}")
+@app.put("/users/{id}")
 def update_user(user_id: int):
     pass
 
 
-@app.patch("{BASE_API_URL}/users/{id}")
+@app.patch("/users/{id}")
 def update_user_password(user_id: int):
     pass
 
 
-@app.delete("{BASE_API_URL}/users/{id}")
+@app.delete("/users/{id}")
 def delete_user(user_id: int):
     pass
--- readme.md
+++ readme.md
@@ -12,56 +12,88 @@
 
 [Keycloak][1]
 
+[Swagger][3]
+
 ## Rotas
+
+### Docs
+
+```http
+GET http://{{base_api_url}}/docs HTTP/1.1
+```
 
 ### Login
 
 ```http
-POST http://localhost:8000 HTTP/1.1
-Authorization: Bearer {{access_token}}
+POST http://{{base_api_url}}/login HTTP/1.1
+Content-type: application/json
+
+{
+  "name": "sample",
+  "time": "Wed, 21 Oct 2015 18:27:50 GMT"
+}
 ```
 
 ### Create user
 
 ```http
-POST http://localhost:8000 HTTP/1.1
+POST http://{{base_api_url}}/users HTTP/1.1
 Authorization: Bearer {{access_token}}
+Content-type: application/json
+
+{
+  "name": "sample",
+  "time": "Wed, 21 Oct 2015 18:27:50 GMT"
+}
 ```
 
 ### Get all users
 
 ```http
-GET http://localhost:8000 HTTP/1.1
+GET http://{{base_api_url}}/users HTTP/1.1
 Authorization: Bearer {{access_token}}
 ```
 
 ### Get user by ID
 
 ```http
-GET http://localhost:8000 HTTP/1.1
+GET http://{{base_api_url}}/users HTTP/1.1
 Authorization: Bearer {{access_token}}
 ```
 
 ### Update user by ID
 
 ```http
-PUT http://localhost:8000 HTTP/1.1
+PUT http://{{base_api_url}}/users HTTP/1.1
 Authorization: Bearer {{access_token}}
+Content-type: application/json
+
+{
+  "name": "sample",
+  "time": "Wed, 21 Oct 2015 18:27:50 GMT"
+}
 ```
 
 ### Update user password
 
 ```http
-PATCH http://localhost:8000 HTTP/1.1
+PATCH http://{{base_api_url}}/users HTTP/1.1
 Authorization: Bearer {{access_token}}
+Content-type: application/json
+
+{
+  "name": "sample",
+  "time": "Wed, 21 Oct 2015 18:27:50 GMT"
+}
 ```
 
 ### Delete user by ID
 
 ```http
-DELETE http:/{{base-api-url}}/users/{id} HTTP/1.1
+DELETE http:/{{base_api_url}}/users/{id} HTTP/1.1
 Authorization: Bearer {{access_token}}
 ```
 
 [0]: https://fastapi.tiangolo.com/ "FastAPI"
 [1]: https://www.keycloak.org/ "Keycloak"
+[2]: https://swagger.io/ "Swagger"
